2a485efa1642642beaaa8a4824b76e249f277a28
brira123 <39010769+brira123@users.noreply.github.com>
2018-09-26 06:30:29 -1000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $.ajax({
-    url: `https://newsapi.org/v2/everything?sources=abc-news&apiKey=7f118cda5fb54966bc7747fea4b5a19b`,
+    url: `https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=41fae6ba68b84d5fa1a29c9c32eed6d9`,
     success: function(data) {
         console.log(data);
-        for (i = 0; i <= 7; i++) {
+        for (i = 0; i < 7; i++) {
             document.querySelector(".news-story").innerHTML +=
                 `
              <div>
                 <h1> ${data.articles[i].title} </h1>
               
                 <p> ${data.articles[i].description}</p>
-            </div class="head-line">
+            </div>
             <div class="news-image" style="background-image: url(${data.articles[i].urlToImage})"></div>
     
         </div>`
